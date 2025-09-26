d2c878ce50bbf87fb87f75171f2be23c66e871ef
Vinay0012 <36453212+Vinay0012@users.noreply.github.com>
2025-09-26 16:15:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import multer from "multer";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import fetch from "node-fetch";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
